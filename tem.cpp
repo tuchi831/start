b80d4b2ee1cf1f396a0b6ee6c96b975dd5d165ca
Tuchi <3277386697@qq.com>
2024-02-16 17:14:24 +0800

    版本1

---
+++ tem.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#define ll long long 
+#define endl '\n'
+const int maxn = 5e5+1000;
+using namespace std;
+int main()
+{
+    int a,b;
+    a = 4;
+    b = 5;
+    
+    return 0;
+}
